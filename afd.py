d8dca617efc7bb5e916c9a8e3b15c6581095d585
danielalexis <danielalexis.o.pereira@gmail.com>
2024-04-07 17:37:24 +0100

    Add reconhecedor function to afd.py and corresponding tests in afd_test.py

--- afd.py
+++ afd.py
@@ -4,3 +4,17 @@ def check_alphabet(afd: dict, word: str) -> bool:
         if char not in alphabet:
             return False
     return True
+
+def reconhecedor(afd: dict, word: str) -> bool:
+    word = word.replace("ε", "")
+    estado_atual = afd["q0"]
+    for char in word:
+        if char in afd["delta"][estado_atual]:
+            estado_atual = afd["delta"][estado_atual][char]
+        elif "ε" in afd["delta"][estado_atual]:
+            estado_atual_aux = afd["delta"][estado_atual]["ε"]
+            if char in afd["delta"][estado_atual_aux]:
+                estado_atual = afd["delta"][estado_atual_aux][char]
+        else:
+            return False
+    return estado_atual in afd["F"]
--- afd_test.py
+++ afd_test.py
@@ -12,3 +12,15 @@ def test_afd_word_not_in_alphabet():
     with open("exemplos/afd.json", "r") as file:
         afd_dict = json.load(file)
     assert afd.check_alphabet(afd_dict, "aaaaccbb") == False, "Should be False"
+
+def test_afd_reconhecedor():
+    afd_dict: dict = {}
+    with open("exemplos/afd.json", "r") as file:
+        afd_dict = json.load(file)
+    assert afd.reconhecedor(afd_dict, "abbb") == True, "Should be True"
+
+def test_afd_reconhecedor_false():
+    afd_dict: dict = {}
+    with open("exemplos/afd.json", "r") as file:
+        afd_dict = json.load(file)
+    assert afd.reconhecedor(afd_dict, "aabb") == False, "Should be False"
