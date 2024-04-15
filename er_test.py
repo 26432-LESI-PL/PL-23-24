d3dd79ae29871678581d56014480c8bebc48ce2c
al26014 <120516305+al26014@users.noreply.github.com>
2024-04-15 20:42:16 +0100

    Refactor er_test.py to add new test cases for re_to_nfa and nfa_to_json functions

--- er_test.py
+++ er_test.py
@@ -4,17 +4,42 @@ import os
 import pytest
 
 def setup_module(module):
-    global afd_dict
-    afd_dict = {}
-    with open("exemplos/afd.json", "r") as file:
-        afd_dict = json.load(file)
+    if os.path.exists("exemplos/nfa.json"):
+        os.remove("exemplos/nfa.json")
 
 def test_re_to_nfa():
-    result, char = afd.check_alphabet(afd_dict, "aaaaabbbb")
-    assert result == True, "Should be True"
-    assert char == "", "Should be empty"
+    re = {
+        "op": "seq",
+        "args": [
+            {
+                "op": "alt",
+                "args": [
+                    {"simb": "a"},
+                    {"simb": "b"}
+                ]
+            },
+            {
+                "simb": "c"
+            }
+        ]
+    }
+    nfa = afd.re_to_nfa(re)
+    assert nfa == "(a|b)c", "Should be (a|b)c"
 
 def test_nfa_to_json():
-    result, char = afd.check_alphabet(afd_dict, "aaaaccbb")
-    assert result == False, "Should be False"
-    assert char == "c", "Should be c"
+    nfa = {
+        "Q": ["q0", "q1", "q2"],
+        "V": ["a", "b"],
+        "q0": "q0",
+        "F": ["q2"],
+        "delta": {
+            "q0": {"a": "q1", "b": "q2"},
+            "q1": {"a": "q1", "b": "q2"},
+            "q2": {"a": "q2", "b": "q2"}
+        }
+    }
+    afd.nfa_to_json(nfa, "exemplos/nfa.json")
+    assert os.path.exists("exemplos/nfa.json"), "File should exist"
+    with open("exemplos/nfa.json", "r") as file:
+        nfa_file = json.load(file)
+    assert nfa == nfa_file, "Should be equal"
