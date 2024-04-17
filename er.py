13dd4dd3c301f39980b4bf829d0648a481b30487
danielalexis <danielalexis.o.pereira@gmail.com>
2024-04-17 17:23:57 +0100

    Refactor ER module functions and test cases, and add AFND to AFND conversion function

--- er.py
+++ er.py
@@ -1,70 +1,62 @@
-import json
-from graphviz import Digraph
-
-def regex_to_nfa(regex: dict):
+def to_afnd(regex):
     if "simb" in regex:
-        return {"Q": ["q0", "q1"], "V": [regex["simb"]], "q0": "q0", "F": ["q1"], "delta": {"q0": {"simb": regex["simb"], "next": "q1"}}}
+        start_state = "q1"
+        end_state = "q2"
+        return {
+            "Q": [start_state, end_state],
+            "V": [regex["simb"]],
+            "q0": start_state,
+            "F": [end_state],
+            "delta": {
+                start_state: {regex["simb"]: [end_state]}
+            }
+        }
+    elif regex["op"] == "alt":
+        nfa1 = to_afnd(regex["args"][0])
+        nfa2 = to_afnd(regex["args"][1])
+        start_state = "q" + str(len(nfa1["Q"]) + len(nfa2["Q"]) + 1)
+        end_state = "q" + str(len(nfa1["Q"]) + len(nfa2["Q"]) + 2)
+        return {
+            "Q": nfa1["Q"] + nfa2["Q"] + [start_state, end_state],
+            "V": list(set(nfa1["V"] + nfa2["V"])),
+            "q0": start_state,
+            "F": [end_state],
+            "delta": {
+                **nfa1["delta"],
+                **nfa2["delta"],
+                start_state: {"ε": [nfa1["q0"], nfa2["q0"]]},
+                nfa1["F"][0]: {"ε": [end_state]},
+                nfa2["F"][0]: {"ε": [end_state]}
+            }
+        }
+    elif regex["op"] == "seq":
+        nfa1 = to_afnd(regex["args"][0])
+        nfa2 = to_afnd(regex["args"][1])
+        return {
+            "Q": nfa1["Q"] + nfa2["Q"],
+            "V": list(set(nfa1["V"] + nfa2["V"])),
+            "q0": nfa1["q0"],
+            "F": nfa2["F"],
+            "delta": {
+                **nfa1["delta"],
+                **nfa2["delta"],
+                nfa1["F"][0]: {"ε": [nfa2["q0"]]}
+            }
+        }
+    elif regex["op"] == "kle":
+        nfa = to_afnd(regex["args"][0])
+        start_state = "q" + str(len(nfa["Q"]) + 1)
+        end_state = "q" + str(len(nfa["Q"]) + 2)
+        return {
+            "Q": nfa["Q"] + [start_state, end_state],
+            "V": nfa["V"],
+            "q0": start_state,
+            "F": [end_state],
+            "delta": {
+                **nfa["delta"],
+                start_state: {"ε": [nfa["q0"], end_state]},
+                nfa["F"][0]: {"ε": [nfa["q0"], end_state]}
+            }
+        }
     else:
-        op = regex["op"]
-        args = regex["args"]
-        if op == "alt":
-            nfa1 = regex_to_nfa(args[0])
-            nfa2 = regex_to_nfa(args[1])
-            return alt_nfa(nfa1, nfa2)
-        elif op == "seq":
-            nfa1 = regex_to_nfa(args[0])
-            nfa2 = regex_to_nfa(args[1])
-            return seq_nfa(nfa1, nfa2)
-        elif op == "kle":
-            nfa = regex_to_nfa(args[0])
-            return kle_nfa(nfa)
-
-def alt_nfa(nfa1: dict, nfa2: dict) -> dict:
-    Q = list(set(["q0"] + nfa1["Q"] + nfa2["Q"]))
-    V = list(set(nfa1["V"] + nfa2["V"]))
-    q0 = "q0"
-    F = list(set(nfa1["F"] + nfa2["F"]))
-    delta = {"q0": {"op": "alt", "next": [nfa1["q0"], nfa2["q0"]]}}
-
-    delta.update(nfa1["delta"])
-    delta.update(nfa2["delta"])
-    return {"Q": Q, "V": V, "q0": q0, "F": F, "delta": delta}
-
-def seq_nfa(nfa1: dict, nfa2: dict) -> dict:
-    Q = list(set(nfa1["Q"] + nfa2["Q"]))
-    V = list(set(nfa1["V"] + nfa2["V"]))
-    q0 = nfa1["q0"]
-    F = nfa2["F"]
-    delta = nfa1["delta"]
-    delta.update(nfa2["delta"])
-    delta[nfa1["F"][0]] = {"op": "seq", "next": nfa2["q0"]}
-    return {"Q": Q, "V": V, "q0": q0, "F": F, "delta": delta}
-
-def kle_nfa(nfa: dict) -> dict:
-    Q = list(set(["q0"] + nfa["Q"]))
-    V = list(set(nfa["V"]))
-    q0 = "q0"
-    F = list(set(nfa["F"] + ["q0"]))
-    delta = {"q0": {"op": "kle", "next": nfa["q0"]}}
-    delta.update(nfa["delta"])
-    delta[nfa["F"][0]] = {"op": "kle", "next": nfa["q0"]}
-    return {"Q": Q, "V": V, "q0": q0, "F": F, "delta": delta}
-
-def afnd_json(nfa: dict, filename: str):
-    with open(filename, "w") as file:
-        json.dump(nfa, file)
-
-def graphviz(nfa: dict):
-    dot = Digraph(comment='Automato Finito Nao Deterministico')
-    dot.node('start', shape='none', label='')
-    dot.edge('start', nfa["q0"], label='')
-    for state in nfa["delta"].keys():
-        if state in nfa["F"]:
-            dot.node(state, state, shape="doublecircle")
-        else:
-            dot.node(state, state, shape="circle")
-    for estado_inicial, transitions in nfa["delta"].items():
-        for simbolo, estados_finais in transitions.items():
-            for estado_final in estados_finais:  # handle the case where estado_final is a list
-                dot.edge(estado_inicial, estado_final, label = simbolo)
-    dot.render('automaton_graph_2', view=True, format='png')
+        raise Exception("Operação inválida")
--- er_test.py
+++ er_test.py
@@ -3,30 +3,9 @@ import er
 import os
 
 er_dict: dict = {}
-with open("exemplos/afnd.json", "r") as file:
+with open("exemplos/exemplo01.er.json", "r") as file:
     er_dict = json.load(file)
 
-def test_regex_to_nfa():
-    regex = {
-        "op": "alt",
-        "args": [
-            {"simb": "a"},
-            {
-                "op": "seq",
-                "args": [
-                    {"simb": "a"},
-                    {"op": "kle", "args": [{"simb": "b"}]}
-                ]
-            }
-        ]
-    }
-    nfa = er.regex_to_nfa(regex)
-    if nfa is not None:
-            er.afnd_json(nfa, "afnd.json")
-    with open("afnd.json", "r") as file:
-            nfa_file = json.load(file)
-    assert nfa == nfa_file, "Should be equal"
-
-def test_er_graphviz():
-    er.graphviz(er_dict)
-    assert os.path.isfile("automaton_graph_2.png") == True, "Should be True"
+def test_er_to_afnd():
+    afnd = er.to_afnd(er_dict)
+    assert isinstance(afnd, dict), "Should be a dictionary"
