44945af4035aa089a65dc01850d7d2b5a8d730eb
danielalexis <danielalexis.o.pereira@gmail.com>
2024-04-17 13:31:38 +0100

    Refactor AFND module functions and test cases, and add AFD conversion function

--- afnd.py
+++ afnd.py
@@ -1,3 +1,4 @@
+import json
 from graphviz import Digraph
 
 def graphviz(afnd: dict):
@@ -15,3 +16,35 @@ def graphviz(afnd: dict):
             for next_state in afnd["delta"][state][transition]:
                 dot.edge(state, next_state, label=transition)
     dot.render("afnd_graph", view=True, format="png")
+
+def convert_to_afd(afnd: dict) -> dict:
+    afd = {
+        "Q": [],
+        "V": afnd["V"],
+        "q0": afnd["q0"],
+        "F": [],
+        "delta": {}
+    }
+    # Adiciona os estados do AFND ao AFD
+    for state in afnd["Q"]:
+        afd["Q"].append(state)
+        afd["delta"][state] = {}
+    # Adiciona os estados finais do AFND ao AFD
+    for state in afnd["F"]:
+        afd["F"].append(state)
+    # Adiciona as transições do AFND ao AFD
+    for state in afnd["Q"]:
+        if state in afnd["delta"]:
+            for transition in afnd["delta"][state].keys():
+                for next_state in afnd["delta"][state][transition]:
+                    if transition not in afd["delta"][state]:
+                        afd["delta"][state][transition] = next_state
+                    else:
+                        afd["delta"][state][transition] += next_state
+    return afd
+
+
+afd_dict: dict = {}
+with open("exemplos/afnd.json", "r", encoding="utf8") as file:
+    afd_dict = json.load(file)
+print(convert_to_afd(afd_dict))
--- afnd_test.py
+++ afnd_test.py
@@ -2,10 +2,16 @@ import json
 import os
 import afnd
 
-afd_dict: dict = {}
+afnd_dict: dict = {}
 with open("exemplos/afnd.json", "r", encoding="utf8") as file:
-    afd_dict = json.load(file)
+    afnd_dict = json.load(file)
 
 def test_afnd_graphviz():
-    afnd.graphviz(afd_dict)
+    afnd.graphviz(afnd_dict)
     assert os.path.isfile("afnd_graph.png") == True, "Should be True"
+
+def test_convert_to_afd():
+    afd = afnd.convert_to_afd(afnd_dict)
+    with open("afd_output.json", "w", encoding="utf8") as file:
+        json.dump(afd, file)
+    assert os.path.isfile("afnd_graph.png"), "Should be True"
